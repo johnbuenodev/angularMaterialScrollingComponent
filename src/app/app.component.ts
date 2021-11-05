c8caa1d2d29d3c6eba9ac7da867c334d87e88883
johnbuenodev <johnbuenodev@gmail.com>
2021-11-04 22:10:17 -0300

    Component base Scrolling para uso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appScrolling';
+
+  listNumber: Array<number> = [];
+  //DECLARAR CORRETAMENTE O VALOR(TIPO, OBJETO) SENÃO O CDK VIRTUAL SCROLLING BUGA
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+    
+      for(let i = 1; i <= 300; i++){ 
+        this.listNumber.push(i);
+      } 
+
+  }
+
+  actionButton(value1:any,value :any){
+    if(value1 == 'btn1'){
+      alert("Pegando valor direto da interpolação sendo passada na função: " + value);
+    } else {
+      let valor = value; 
+      alert("Pegando valor pelo template variavel: " + valor.value);
+    }
+  }
+
 }
+
+
+/*
+
+ng add @angular/material
+
+*/
